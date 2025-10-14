cda4874002627af1ec1563830a054d8855b984c6
jswj30 <jswj30@naver.com>
2025-10-14 11:04:00 +0900

    SUIT custom font 적용

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigation from "./navigations/RootNavigation";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "SUIT-Regular": require("./assets/fonts/SUIT-Regular.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <RootNavigation />
