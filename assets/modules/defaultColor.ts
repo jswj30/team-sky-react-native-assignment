60ffc601473ceb253e956b9127dea0bc02b513db
jswj30 <jswj30@naver.com>
2025-10-14 11:04:53 +0900

    bottom tabs 디자인 수정

---
+++ assets/modules/defaultColor.ts
@@ -0,0 +1,6 @@
+export const defaultColor = {
+  black: "#3c3c3c",
+  white: "#ffffff",
+  gray1: "#c4c4c4",
+  bottomTabTopWidth: "#D7D7D7",
+};
--- navigations/BottomTabNavigation.tsx
+++ navigations/BottomTabNavigation.tsx
@@ -1,19 +1,91 @@
+import { Image, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { BottomTabParamList } from "../types/type_list";
+import { RouteProp } from "@react-navigation/native";
+import { defaultColor } from "../assets/modules/defaultColor";
 import LearningScreen from "../screens/learning/LearningScreen";
 import MyStudyScreen from "../screens/myStudy/MyStudyScreen";
+import learningIcon from "../assets/images/bottomTab/learning_icon.png";
+import learningIconFocused from "../assets/images/bottomTab/learning_icon_focused.png";
+import myStudyIcon from "../assets/images/bottomTab/my_study_icon.png";
+import myStudyIconFocused from "../assets/images/bottomTab/my_study_icon_focused.png";
 
 const Tab = createBottomTabNavigator<BottomTabParamList>();
+
+const tabBarIcon = (
+  route: RouteProp<BottomTabParamList, keyof BottomTabParamList>,
+  focused: boolean
+) => {
+  if (route.name === "Learning") {
+    return (
+      <Image
+        source={focused ? learningIconFocused : learningIcon}
+        resizeMode="contain"
+        style={[styles.icon]}
+      />
+    );
+  }
+
+  if (route.name === "MyStudy") {
+    return (
+      <Image
+        source={focused ? myStudyIconFocused : myStudyIcon}
+        resizeMode="contain"
+        style={[styles.icon]}
+      />
+    );
+  }
+
+  return <></>;
+};
 
 export default function BottomTabNavigation() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({
+        route,
+      }: {
+        route: RouteProp<BottomTabParamList, keyof BottomTabParamList>;
+      }) => ({
         headerShown: false,
-      }}
+        tabBarStyle: styles.tabBarStyle,
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+        tabBarActiveTintColor: defaultColor.black,
+        tabBarInactiveTintColor: defaultColor.gray1,
+        tabBarIcon: ({ focused }: { focused: boolean }) =>
+          tabBarIcon(route, focused),
+      })}
     >
-      <Tab.Screen name="Learning" component={LearningScreen} />
-      <Tab.Screen name="MyStudy" component={MyStudyScreen} />
+      <Tab.Screen
+        name="Learning"
+        options={{
+          title: "학습",
+        }}
+        component={LearningScreen}
+      />
+      <Tab.Screen
+        name="MyStudy"
+        options={{
+          title: "나의 서재",
+        }}
+        component={MyStudyScreen}
+      />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    elevation: 0,
+    borderTopWidth: 1,
+    borderTopColor: defaultColor.bottomTabTopWidth,
+  },
+  tabBarLabelStyle: {
+    fontFamily: "SUIT-Regular",
+    fontSize: 10,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
